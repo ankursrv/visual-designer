71f90df7ac402b8c6d57cd72edae194479a6731e
ankur srivastava <ankur.srivastava@techchefz.com>
2024-07-22 17:51:13 +0530

    about page modal creation

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -19,8 +19,10 @@ const recipes = [
 
 ]
 
+
+
 function About() {
-    const [open, setOpen] = useState(true)
+    const [open, setOpen] = useState(false)
     return (
         <>
             {
@@ -28,7 +30,10 @@ function About() {
                     <div>
                         <h1>{recipeItem.id}</h1>
                         <p>{recipeItem.title}</p>
-                        <p><img src={recipeItem.image} className="w-8" /></p>
+                        <p onClick={() => setOpen(true)} className="flex items-center">      {/***** //MOdal *****/}
+                            <img src={recipeItem.image} className="w-24" />
+                            <h2 className="text-5xl font-bold">MODAL</h2>
+                        </p>
                         {
                             recipeItem.dishTypes.map((dish, index) => (
                                 <h3>{dish}</h3>
@@ -39,11 +44,23 @@ function About() {
             }
 
             {/* MOdal Start */}
-            <div className="">
-            <div className="absolute w-full h-full bg-green top-0 left-auto"></div>
+            {
+                open && (
 
-            </div>
+                    <div className="flex items-center justify-center h-full">
+                        <div className="absolute w-full h-full bg-black top-0 left-auto opacity-60"></div>
+                        <div className="absolute z-50  bg-white w-2/6 top-2/4 -translate-y-2/4">
+                            <button className="bg-green text-3xl w-12 h-12 pb-2 ml-auto block" onClick={() => setOpen(false)}>×</button>
+                            <div className="p-5">
+                                <h1>Modal </h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
             {/* MOdal END  */}
+
 
 
         </>
