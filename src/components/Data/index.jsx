c7bc71f73f23badc8c1521222119ad8ecc08b5d9
ankur srivastava <ankur.srivastava@techchefz.com>
2024-08-09 15:49:24 +0530

    Data componenet

---
+++ src/components/Data/index.jsx
@@ -0,0 +1,145 @@
+    // Hero Banner start 
+    const heroSlider = [
+        {
+            title: "Branding | Image making",
+            heading: "Visual Designer",
+            paragraph: "This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com",
+            image: "/images/heroimg.webp"
+        },
+        {
+            title: "Branding | Image making",
+            heading: "UX/UI Designer",
+            paragraph: "This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com",
+            image: "/images/heroimg.webp"
+        }
+    ];
+    // Hero Banner END 
+
+    // Brand Slider start 
+const brandImage = [
+    {   
+        imageBrand: 'brandimg1.webp',
+        altText: 'brand image',
+    },
+    {
+        imageBrand: 'brandimg2.webp',
+        altText: 'brand image',
+    },
+    {
+        imageBrand: 'brandimg3.webp',
+        altText: 'brand image',
+    },
+    {
+        imageBrand: 'brandimg4.webp',
+        altText: 'brand image',
+    },
+    {
+        imageBrand: 'brandimg5.webp',
+        altText: 'brand image',
+    },
+    {
+        imageBrand: 'brandimg1.webp',
+        altText: 'brand image',
+    }
+
+];
+// Brand Slider END 
+
+// Design Section start
+const designCard = [
+    {
+        designImage: 'designimg1.webp',
+        designTitle: 'Product Design',
+        designDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com'
+    },
+    {
+        designImage: 'designimg2.webp',
+        designTitle: 'Visual Design',
+        designDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com'
+    },
+    {
+        designImage: 'designimg3.webp',
+        designTitle: 'Art Direction',
+        designDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com'
+    }
+];
+// Design Section END
+
+// Latest Work Section start 
+const latestWrok = [
+    {
+        workImage: 'workimg1.webp',
+        workTitle: 'Project title',
+        workDepartment: 'UI, Art drection'
+    },
+    {
+        workImage: 'workimg2.webp',
+        workTitle: 'Project title',
+        workDepartment: 'UI, Art drection'
+    },
+    {
+        workImage: 'workimg3.webp',
+        workTitle: 'Project title',
+        workDepartment: 'UI, Art drection'
+    },
+    {
+        workImage: 'workimg4.webp',
+        workTitle: 'Project title',
+        workDepartment: 'UI, Art drection'
+    },
+    {
+        workImage: 'workimg5.webp',
+        workTitle: 'Project title',
+        workDepartment: 'UI, Art drection'
+    },
+    {
+        workImage: 'workimg6.webp',
+        workTitle: 'Project title',
+        workDepartment: 'UI, Art drection'
+    }
+];
+
+// Latest Work Section END
+
+// Testimonial Section start 
+const testiCard = [
+    {
+        testiDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com',
+        textiImage: 'testiimg1.webp',
+        testiName: 'Gemma Nolen',
+        testiBrand: 'Google'
+    },
+    {
+        testiDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com',
+        textiImage: 'testiimg1.webp',
+        testiName: 'Gemma Nolen',
+        testiBrand: 'Google'
+    },
+    {
+        testiDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com',
+        textiImage: 'testiimg1.webp',
+        testiName: 'Gemma Nolen',
+        testiBrand: 'Google'
+    },
+    {
+        testiDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com',
+        textiImage: 'testiimg1.webp',
+        testiName: 'Gemma Nolen',
+        testiBrand: 'Google'
+    },
+    {
+        testiDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com',
+        textiImage: 'testiimg1.webp',
+        testiName: 'Gemma Nolen',
+        testiBrand: 'Google'
+    },
+    {
+        testiDiscription: 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com',
+        textiImage: 'testiimg1.webp',
+        testiName: 'Gemma Nolen',
+        testiBrand: 'Google'
+    }
+];
+// Testimonial Section END
+
+    export {heroSlider,brandImage, designCard, latestWrok, testiCard}
